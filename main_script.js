f60d37d60b4c22910742fea40a143e967ca145ba
Jeremy Mossa <121771956+Jeremy-Mossa@users.noreply.github.com>
2024-03-05 19:33:58 -0800

    Delete main_script.js, wrong naming convention at this stage

--- main_script.js
+++
@@ -1,7 +0,0 @@
-const hamburgerButton = document.querySelector('.hamburger-button');
-const navLinks = document.querySelector('.nav-links');
-
-hamburgerButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-  hamburgerButton.classList.toggle('active'); // Optional: Add class for visual feedback on button
-});
