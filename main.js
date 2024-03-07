3c9a352294810126587b8f0c1bcf1bb2b6d9ec87
Jeremy Mossa <121771956+Jeremy-Mossa@users.noreply.github.com>
2024-03-06 19:07:48 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
-const toggleModeBtn = document.querySelector('.toggle-mode');
-const hamburgerBtn = document.querySelector('.hamburger');
-const dropdown = document.querySelector('.dropdown');
+const themeSwitch = document.getElementById('theme-switch');
+const body = document.body;
 
-// Event listener for toggle mode button
-toggleModeBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
-
-// Event listener for hamburger button
-hamburgerBtn.addEventListener('click', () => {
-  // Toggle dropdown visibility
-  dropdown.classList.toggle('active');
-});
-
-// Close dropdown when clicking outside of it
-document.addEventListener('click', (event) => {
-  if (!event.target.matches('.hamburger') && !event.target.closest('.dropdown')) {
-    dropdown.classList.remove('active');
-  }
+themeSwitch.addEventListener('change', function() {
+    body.classList.toggle('dark-mode');
 });
