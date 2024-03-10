157009f882e0e8035a0491316dc17d7f7cb3d620
Jeremy Mossa <121771956+Jeremy-Mossa@users.noreply.github.com>
2024-03-09 19:52:23 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-const themeSwitch = document.getElementById('theme-switch');
-const body = document.body;
 
-themeSwitch.addEventListener('change', function() {
-    body.classList.toggle('dark-mode');
-});
