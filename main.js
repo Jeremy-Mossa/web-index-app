802ce768388aaef1602877ca358cb9b7388c5419
Jeremy-Mossa <jeremy.mossa@gmail.com>
2024-03-25 20:24:43 -0700

    javascript practice before bed

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-if (5 >= 2) {
-    console.log("it's truth")
+const d = new Date()
+const hour = d.getHours()
+
+console.log(hour)
+
+if (5 < hour & hour < 12) {
+    console.log('good morning')
+} else if (13 < hour & hour < 17) {
+    console.log('good afternoon')
+} else if (18 < hour & hour < 24) {
+    console.log('good evening')
 }
-
-const a=3
-const b='3'
-
-if (a == b) 
-    console.log('tru')
-else 
-    console.log('falsity')
-
