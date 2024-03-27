c20ce677770ab4551f2a4055371da01c32eb28bb
Jeremy-Mossa <jeremy.mossa@gmail.com>
2024-03-26 19:45:07 -0700

    javascript practice

--- main.js
+++ main.js
@@ -4,8 +4,41 @@
 //    console.log(item)
 //}
 
-const str = 'hey there'
+//const str = 'hey there'
+//
+//for (const letter of str) {
+//    console.log(letter)
+//}
+//
+//const map = new Map()
+//map.set('name', 'jay')
+//map.set('age', '99')
+//
+//for (const [key, value] of map) {
+//    console.log(key, value)
+//}
 
-for (const letter of str) {
-    console.log(letter)
-}
+//const colorObj = {
+//    color1: 'red',
+//    color2: 'orange',
+//    color3: 'yellow',
+//    color4: 'blue',
+//    color5: 'gray',
+//    color6: 'forest green',
+//}
+//
+//for (const key in colorObj) {
+//    console.log(key, colorObj[key])
+//}
+//
+//const colorArr = ['forest green', 'orange', 'blue']
+//for (const key in colorArr) {
+//    console.log(colorArr[key])
+//}
+
+const apps = ['search', 'messages', 'email', 'camera']
+
+//console.log(apps.__proto__)
+
+apps.forEach(element => console.log(element))
+
