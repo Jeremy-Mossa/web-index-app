ee30cb3732771d39289c8180ed25aa5547cb3a0d
Jeremy-Mossa <jeremy.mossa@gmail.com>
2024-03-27 18:19:36 -0700

    javascript practice

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 let containers = ['box', 'bowl', 'cup', 'bag', 'jar']
+containers = containers.sort()
 
-console.log(containers.__proto__)
-containers.forEach((container) => console.log(container))
+//console.log(containers.__proto__)
+//containers.forEach((container) => console.log(container))
+
+function log_containers(container) {
+    console.log(container)
+}
+
+containers.forEach(log_containers)
+
+console.log(containers)
